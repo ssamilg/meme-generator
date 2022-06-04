6875140d9f55a2245930a7f5ffc678a4aef98700
ssamilg <samil-98@hotmail.com>
2022-06-05 02:32:42 +0300

    contributor links added to footer

--- components/MgFooter.js
+++ components/MgFooter.js
@@ -5,13 +5,24 @@ export default function MgFooter() {
   return (
     <>
       <footer className={styles.footer}>
-          Developed by{' '}
-          <b>SSG</b>
-
-        {/* 
+        {/* <div className='flex-row'>
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />     
-        */}
+        </div> */}
+
+        <div className='flex-row'>
+          <div>
+            Using{' '}
+            <a href='https://github.com/D3vd/Meme_Api'>Meme-api</a>
+          </div>
+        </div>
+
+        <div className='flex-row'>
+          <div className={styles.ssgSignature}>
+            Developed by{' '}
+            <a href='https://github.com/ssamilg'>SSG</a>
+          </div>
+        </div>
       </footer>
     </>
   )
