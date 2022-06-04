f50609b143598b1c723204fb399dedb43517e80f
ssamilg <samil-98@hotmail.com>
2022-06-05 02:19:02 +0300

    unused codes removed

--- components/MgFooter.js
+++ components/MgFooter.js
@@ -3,15 +3,16 @@ import styles from '../styles/Home.module.css';
 
 export default function MgFooter() {
   return (
-    <footer className={styles.footer}>
-      <div className='flex-row'>
-        Developed by SSG
-      </div>
+    <>
+      <footer className={styles.footer}>
+          Developed by{' '}
+          <b>SSG</b>
 
-      {/* <div className='flex-row'>
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />     
-      </div> */}
-    </footer>
+        {/* 
+          Powered by{' '}
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />     
+        */}
+      </footer>
+    </>
   )
 }
